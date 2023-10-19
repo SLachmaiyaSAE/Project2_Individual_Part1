9614c5f99de8c903511153cc7dd0355702a989d0
SLachmaiyaSAE <136556179+SLachmaiyaSAE@users.noreply.github.com>
2023-10-19 17:13:35 +1100

    Initial Upload

---
+++ Assets/Scripts/GroundSpawnScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawnScript : MonoBehaviour
+{
+    public GameObject ground;
+    public float spawnRate = 10;
+    private float timer = 0;
+    public float heightOffset = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+
+        else
+        {
+            spawnGround();
+            timer = 0;
+        }
+    }
+    void spawnGround()
+    { 
+        {
+            float lowestPoint = transform.position.y - heightOffset;
+            float highestPoint = transform.position.y + heightOffset;
+
+            Instantiate(ground, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float moveStrength;
+    public float horizontal; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector3.up * moveStrength;
+        }
+       
+        if(Input.GetKeyDown(KeyCode.D) == true)
+        {
+            myRigidbody.velocity = Vector2.right * moveStrength;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
+            myRigidbody.velocity = Vector2.left * moveStrength;
+        }
+    }
+
+}
